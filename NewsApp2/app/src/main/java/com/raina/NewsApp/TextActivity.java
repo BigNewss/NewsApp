ecc2c1d3ea0073291b4856d7b678d31362cc542d
gconeice <421662885@qq.com>
2017-09-13 15:56:09 +0800

    share sdk

--- NewsApp2/app/src/main/java/com/raina/NewsApp/TextActivity.java
+++ NewsApp2/app/src/main/java/com/raina/NewsApp/TextActivity.java
@@ -47,6 +47,7 @@ import com.iflytek.cloud.SpeechError;
 import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.SynthesizerListener;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 //import com.raina.newsapp2.R;
 
@@ -58,8 +59,31 @@ public class TextActivity extends AppCompatActivity {
     private PopupWindow popupWindow;
 
     private SpeechSynthesizer mySynthesizer;
-    private Button tts_Button;
-    private TextView tts_TextView;
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle(getTitle);
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl(getURL);
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(getIntro);
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl(getPic);
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        //oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        //oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        //oks.setSiteUrl("http://sharesdk.cn");
+        oks.show(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,7 +163,8 @@ public class TextActivity extends AppCompatActivity {
                         saved = !saved;
                         break;
                     case R.id.menu_share:
-                        popupWindow.showAtLocation(findViewById(R.id.menu_share), Gravity.BOTTOM, 0, 0);
+                        showShare();
+                        //popupWindow.showAtLocation(findViewById(R.id.menu_share), Gravity.BOTTOM, 0, 0);
                         break;
                     case R.id.menu_speak:
                         mySynthesizer.setParameter(SpeechConstant.VOICE_NAME,"xiaoyu");
@@ -201,10 +226,17 @@ public class TextActivity extends AppCompatActivity {
 
     String body;
     String pureText;
+    String getTitle;
+    String getIntro;
+    String getPic;
+    String getURL;
 
     private void initContent() {
         Intent intent = getIntent();
         final String title = intent.getStringExtra("Title");
+        getTitle = title;
+        getIntro = intent.getStringExtra("Intro");
+        getURL = intent.getStringExtra("URL");
         final String keyWords = intent.getStringExtra("keywords");
         body = intent.getStringExtra("Body");
         pureText = body.replaceAll("<(\\s|\\S)*?>", "");
@@ -238,6 +270,7 @@ public class TextActivity extends AppCompatActivity {
                 try {
                     //imageView.setImageBitmap(returnBitMap(imageList[0]));
                     final String get_path = imageList[0];
+                    getPic = get_path;
                     new Thread(new Runnable() {
 
                         @Override
@@ -283,6 +316,7 @@ public class TextActivity extends AppCompatActivity {
                             String get_path = "";
                             try {
                                 get_path = GetPictures.getURL(keyWords);
+                                getPic = get_path;
                             } catch (Exception e) {}
                             final Bitmap bitmap=returnBitMap(get_path);
 
